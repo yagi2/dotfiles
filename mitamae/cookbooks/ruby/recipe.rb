04f1fd5f88e3fc76bb0b4047a1808c55f0c0446d
Itsuki Aoyagi <mail@yagi2.dev>
2021-09-01 20:48:21 +0900

    add condition to install latest ruby

--- mitamae/cookbooks/ruby/recipe.rb
+++ mitamae/cookbooks/ruby/recipe.rb
@@ -1,6 +1,8 @@
 package 'rbenv'
 
 execute 'install latest ruby' do
+    not_if "rbenv global $(rbenv install -L | grep -v - | tail -1)"
+    
     command <<~SCRIPT
     rbenv install $(rbenv install -L | grep -v - | tail -1)
     rbenv global $(rbenv install -L | grep -v - | tail -1)
