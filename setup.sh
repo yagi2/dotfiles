faa00f67bc6a3ba529a0acb17f8c5ca11a8158c2
Itsuki Aoyagi <mail@yagi2.dev>
2021-11-19 10:11:52 +0900

    add missing doller charactor

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ arch=$(uname -m)
 
 sudo mkdir /usr/local/bin
 
-if [ arch = "arm64" ] ; then
+if [ $arch = "arm64" ] ; then
     sudo curl -fsSLo /usr/local/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/latest/download/mitamae-aarch64-darwin
 else 
     sudo curl -fsSLo /usr/local/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/latest/download/mitamae-x86_64-darwin
