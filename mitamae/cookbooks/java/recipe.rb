1d2e601b5550c8f25798bcb30b5ed4d72cbed86b
Itsuki Aoyagi <mail@yagi2.dev>
2021-11-19 12:15:42 +0900

    test modification

--- mitamae/cookbooks/java/recipe.rb
+++ mitamae/cookbooks/java/recipe.rb
@@ -1,6 +1,14 @@
 package 'openjdk@11'
 
-execute 'create symbolic-link' do
-    command "ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk"
-    user 'root'
+if node[:arch] == "arm64"
+    # execute 'create symbolic-link' do
+    #     command "ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk"
+    #     user 'root'
+    # end
+    puts "ここにきたよ"
+else
+    execute 'create symbolic-link' do
+        command "ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk"
+        user 'root'
+    end
 end
