6437a0ce73aac50dd04501991884b102d2fde364
Itsuki Aoyagi <mail@yagi2.dev>
2021-11-19 12:27:30 +0900

    implement different cases of symbolic links depending on the architecture.

--- mitamae/cookbooks/java/recipe.rb
+++ mitamae/cookbooks/java/recipe.rb
@@ -1,11 +1,10 @@
 # package 'openjdk@11'
 
 if node[:arch] == "arm64"
-    # execute 'create symbolic-link' do
-    #     command "ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk"
-    #     user 'root'
-    # end
-    puts "ここにきたよ"
+    execute 'create symbolic-link' do
+        command "ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk"
+        user 'root'
+    end
 else
     execute 'create symbolic-link' do
         command "ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk"
